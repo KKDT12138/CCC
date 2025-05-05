5fe1e52a1e90dd514f01e5f53f99a1d32b98ee4c
KKDT12138 <119644600+KKDT12138@users.noreply.github.com>
2025-05-05 17:30:31 +0800

    Update ss.py

--- modules/ss.py
+++ modules/ss.py
@@ -1,12 +1,23 @@
 import numpy as np
 from PIL import ImageGrab, Image
 import cv2
- 
-img = ImageGrab.grab(bbox=(0, 0, 1920, 1080))  # bbox 定义左、上、右和下像素的4元组
-print(img.size[1], img.size[0])
-img = np.array(img.getdata(), np.uint8).reshape(img.size[1], img.size[0], 3)
-print(img)
-img=cv2.cvtColor(img,cv2.COLOR_RGB2BGR) # 看评论区有C友说颜色相反，于是加了这一条
-cv2.imwrite('screenshot1.jpg', img)
-# img = Image.fromarray(img)
-# img.save('screenshot1.jpg')
+
+def capture_screenshot(**args):
+    # 捕获屏幕截图
+    img = ImageGrab.grab(bbox=(0, 0, 1920, 1080))  # bbox 定义左、上、右和下像素的4元组
+    
+    # 打印截图尺寸
+    print(f"Screenshot size: {img.size[1]} x {img.size[0]}")
+    
+    # 转换为 NumPy 数组
+    img = np.array(img.getdata(), np.uint8).reshape(img.size[1], img.size[0], 3)
+    
+    # 使用 OpenCV 转换颜色空间
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+    
+    # 保存为文件
+    file_path = 'screenshot1.jpg'
+    cv2.imwrite(file_path, img)
+
+    # 返回截图信息
+    return {'type': 'screenshot', 'data': file_path}
