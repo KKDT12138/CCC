cbf4d060d25d9f92bfbc788f893c452c856e0159
KKDT12138 <119644600+KKDT12138@users.noreply.github.com>
2025-05-05 18:41:28 +0800

    Update ss.py

--- modules/ss.py
+++ modules/ss.py
@@ -3,6 +3,7 @@ import win32api
 import win32con
 import win32gui
 import win32ui
+from PIL import Image
 
 
 def get_dimensions():
@@ -33,13 +34,26 @@ def screenshot(filename='screenshot.bmp'):
     win32gui.DeleteObject(screenshot.GetHandle())
 
 
+def compress_image(input_filename, output_filename, target_size_kb=300):
+    """ Compress the image to be under the target size (in KB). """
+    img = Image.open(input_filename)
+    quality = 95  # Start with a high quality
+
+    while True:
+        img.save(output_filename, format="JPEG", quality=quality)
+        # Check the size of the file
+        if os.path.getsize(output_filename) <= target_size_kb * 1024 or quality <= 10:
+            break
+        quality -= 5
+
+
 def run():
-    screenshot()
-    with open('screenshot.bmp', 'rb') as f:
+    screenshot('screenshot.bmp')  # Capture the screenshot
+    compress_image('screenshot.bmp', 'screenshot_compressed.jpg')  # Compress it to < 300KB
+    with open('screenshot_compressed.jpg', 'rb') as f:
         img = f.read()
     return img
 
 
-
 if __name__ == '__main__':
-    screenshot()
+    run()
