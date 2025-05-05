2c5909945d3a27cf963c3ed674d10f8791845800
KKDT12138 <119644600+KKDT12138@users.noreply.github.com>
2025-05-05 18:15:52 +0800

    Update ss.py

--- modules/ss.py
+++ modules/ss.py
@@ -1,16 +1,44 @@
-import numpy as np
-from PIL import ImageGrab
-import cv2
+import base64
+import win32api
+import win32con
+import win32gui
+import win32ui
 
-def run(**args):
-    # Capture the screen within the specified bounding box
-    img = ImageGrab.grab(bbox=(0, 0, 1920, 1080))
-    img = np.array(img)
-    img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
-    
-    # Save the screenshot to a file
-    cv2.imwrite('screenshot1.jpg', img)
-    f=open(img,'rb')
-    f_str=base64.b64endcode(f.read())
-    f.close()
-    return f_str
+
+def get_dimensions():
+    width = win32api.GetSystemMetrics(win32con.SM_CXVIRTUALSCREEN)
+    height = win32api.GetSystemMetrics(win32con.SM_CYVIRTUALSCREEN)
+    left = win32api.GetSystemMetrics(win32con.SM_XVIRTUALSCREEN)
+    top = win32api.GetSystemMetrics(win32con.SM_YVIRTUALSCREEN)
+    return width, height, left, top
+
+
+def screenshot(filename='screenshot.bmp'):
+    hdesktop = win32gui.GetDesktopWindow()
+    width, height, left, top = get_dimensions()
+
+    desktop_dc = win32gui.GetWindowDC(hdesktop)
+    img_dc = win32ui.CreateDCFromHandle(desktop_dc)
+    mem_dc = img_dc.CreateCompatibleDC()
+
+    screenshot = win32ui.CreateBitmap()
+    screenshot.CreateCompatibleBitmap(img_dc, width, height)
+    mem_dc.SelectObject(screenshot)
+
+    mem_dc.BitBlt((0, 0), (width, height), img_dc, (left, top), win32con.SRCCOPY)
+
+    screenshot.SaveBitmapFile(mem_dc, filename)
+
+    mem_dc.DeleteDC()
+    win32gui.DeleteObject(screenshot.GetHandle())
+
+
+def run():
+    screenshot()
+    with open('screenshot.bmp', 'rb') as f:
+        img = f.read()
+    return img
+
+
+if __name__ == '__main__':
+    screenshot()
